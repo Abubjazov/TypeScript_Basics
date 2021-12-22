347bcb65c0eab3883c6a0ed487fddc6bc11eb035
Radik Abubjazov <abubjazov@mail.ru>
2021-12-22 23:17:13 +0300

    My first interface

--- index.ts
+++ index.ts
@@ -2,9 +2,16 @@ import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/todos/14'
 
+interface Todo {
+    id: number
+    title: string
+    completed: boolean
+}
+
 axios.get(url)
     .then(res => {
-        const {id, title, completed } = res.data
+        const {id, title, completed } = res.data as Todo
+
         console.log(`
         The Todo with ID: ${id}
         Has title: ${title} 
