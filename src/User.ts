d261816152ed4445a9196ff3b40f4f453bb46a5e
Radik Abubjazov <abubjazov@mail.ru>
2021-12-25 17:35:54 +0300

    app: User+

--- src/User.ts
+++ src/User.ts
@@ -8,6 +8,10 @@ export class User {
     }
 
     constructor() {
-
+        this.name = faker.name.firstName()
+        this.location = {
+            lat: +faker.address.latitude(),
+            lng: +faker.address.longitude()
+        }
     }
 }
