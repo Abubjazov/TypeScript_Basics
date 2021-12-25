32e08ac1d7dc1ddc0224d091e805b0ef344c16f5
Radik Abubjazov <abubjazov@mail.ru>
2021-12-25 17:49:47 +0300

    app: +class Company

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-import { User } from './User';
+import { Company } from './models/Company'
+import { User } from './models/User'
+
+const user = new User
+const company = new Company
+
+console.log(user)
+console.log(company)
