ad5226689b828be90bc7ed5eea3eb59ba28c8a08
Radik Abubjazov <abubjazov@mail.ru>
2021-12-26 17:37:24 +0300

    app: +Google maps API

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,10 @@ import 'dotenv/config'
 import { Company } from './models/Company'
 import { User } from './models/User'
 
-const user = new User
-const company = new Company
+new google.maps.Map(document.getElementById('map'), {
+    center: {
+        lat: 0,
+        lng: 0
+    },
+    zoom: 1
+})
