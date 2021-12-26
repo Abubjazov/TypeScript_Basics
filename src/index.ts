ba46aeec6d328b2fe72de6a95c43f755c31639c9
Radik Abubjazov <abubjazov@mail.ru>
2021-12-26 18:15:46 +0300

    app: CustomMap: +addCompanyMarker()

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,9 @@ import { User } from './models/User'
 import { Company } from './models/Company'
 import { CustomMap } from './models/CustomMap'
 
-new CustomMap('map')
+const map = new CustomMap('map')
+const user = new User()
+const company = new Company()
+
+map.addUserMarker(user)
+map.addUserMarker(company)
